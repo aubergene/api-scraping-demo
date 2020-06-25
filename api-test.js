481ea9579694a1b939296e7d159221f6b2f2c131
Julian Burgess <jburgess29@bloomberg.net>
2020-06-25 10:49:51 +0100

    initial commit

---
+++ api-test.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch')
+
+const cities = ["London", "Brighton", "Liverpool"]
+
+cities.forEach(city => {
+    const url = `https://www.metaweather.com/api/location/search/?query=${city}`
+    console.log(url)
+
+    fetch(url)
+        .then(d => d.json())
+        .then(d => {
+            const city = d[0]
+            console.log(city)
+            const url = `https://www.metaweather.com/api/location/${city.woeid}/`
+            console.log(url)
+
+            fetch(url)
+                .then(d => d.json())
+                .then(d => console.log(d))
+        })
+})
+
